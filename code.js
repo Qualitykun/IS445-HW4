2f18bd7cc04c7bc872082e1a31bc58188cc7672f
Quality.kun <jrgames@kaiohana.net>
2019-10-16 08:21:23 -0700

    attempt 3

--- code.js
+++ code.js
@@ -1,27 +1,31 @@
 var varSize=[3]
 const balloonDiv=document.getElementById('balloonDiv')
 const ballonPara=document.getElementById('balloonPara')
+const balloonSize=ballonPara.style.fontSize
 
 function balloonChange(event) {
-    if (event.code=='KeyD') {
-        balloonPara.style.fontSize=(balloonPara.style.fontSize-10)+'px';
-        varSize-=1;
-    } else if (event.code=='KeyI') {
-        balloonPara.style.fontSize=(balloonPara.style.fontSize+10)+'px';
-        varSize+=1;
-    }  
+    while (varSize>0 && varSize<6) {
+        if (event.code=='KeyD') {
+            balloonSize=(balloonSize-10)+'px';
+            varSize-=1;
+        } else if (event.code=='KeyI') {
+            balloonSize=(balloonSize+10)+'px';
+            varSize+=1;
+        }
+    }
+    if (varSize==0) {
+        balloonSize = "16pt";
+        balloonPara.innerHTML = "Done";
+        balloonDiv.removeEventListener('keyup', balloonChange);
+    } else if (varSize==6) {
+        balloonPara.innerHTML = "💥";
+        balloonDiv.removeEventListener('keyup', balloonChange);
+    }   
+    
 }
 
 document.getElementById('balloonDiv').addEventListener('keyup', balloonChange)
 
-if (varSize==0) {
-    balloonDiv.style.fontSize = "16pt";
-    balloonDiv.innerHTML = "Done";
-    balloonDiv.removeEventListener('keyup', balloonChange);
-} else if (varSize==6) {
-    balloonDiv.innerHTML = "💥";
-    balloonDiv.removeEventListener('keyup', balloonChange);
-}   
 
 document.getElementById('balloonDiv').addEventListener('click', test => {
     console.log('hi')    
