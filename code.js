722dd20e1f10fa35a974656be53ad675ba57b1f8
Quality.kun <jrgames@kaiohana.net>
2019-10-16 06:12:56 -0700

    Attempt 1

--- code.js
+++ code.js
@@ -1,10 +1,33 @@
-function ballon(varSize) {
-    while (varSize>=0 && varSize<=6) {
-        
-    }
-    if (varSize==0) {
-        
-    } else if (varSize==6) {
-        
-    }
+var varSize=[3]
+const balloonDiv = document.getElementById('balloon')
+var balloonSize = balloonDiv.style.fontSize
+function balloonInf(event) {
+    if (event.key=='KeyI') {
+        balloonDiv.style.fontSize=(balloonSize+10)+'px';
+        varSize+=1;
+    } 
 }
+
+function balloonDef(event) {
+    if (event.key=='KeyD') {
+        balloonDiv.style.fontSize=(balloonSize-10)+'px';
+        varSize-=1;
+    }
+    
+}
+
+function balloonEvent() {
+    while (varSize>0 && varSize<6) {
+        balloonDiv.addEventListener('keyup', balloonDef);
+        balloonDiv.addEventListener('keyup', balloonInf);
+    } 
+    if (varSize==0) {
+        balloonDiv.style.fontSize = "16pt";
+        balloonDiv.innerHTML = "Done";
+    } else if (varSize==6) {
+        balloonDiv.innerHTML = "💥"
+    }
+    
+}
+
+balloonEvent();
