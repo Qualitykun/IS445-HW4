6b692d2e57605f84ba4e20901649cd5afcc56d81
Quality.kun <jrgames@kaiohana.net>
2019-10-16 08:42:55 -0700

    new test

--- code.js
+++ code.js
@@ -3,7 +3,7 @@ const balloonDiv=document.getElementById('balloonDiv')
 const ballonPara=document.getElementById('balloonPara')
 const balloonSize=ballonPara.style.fontSize
 
-function balloonChange(event) {
+/*function balloonChange(event) {
     while (varSize>0 && varSize<6) {
         if (event.code=='KeyD') {
             balloonSize=(balloonSize-10)+'px';
@@ -23,9 +23,29 @@ function balloonChange(event) {
         balloonPara.innerHTML = "💥";
         balloonDiv.removeEventListener('keyup', balloonChange);
     }   
-}
+}*/
 
-balloonDiv.addEventListener('keyup', balloonChange)
+balloonDiv.addEventListener('keyup', balloonChange => {
+    while (varSize>0 && varSize<6) {
+        if (event.code=='KeyD') {
+            balloonSize=(balloonSize-10)+'px';
+            varSize-=1;
+            console.log(varSize)
+        } else if (event.code=='KeyI') {
+            balloonSize=(balloonSize+10)+'px';
+            varSize+=1;
+            console.log(varSize)
+        }
+    }
+    if (varSize==0) {
+        balloonSize = "16pt";
+        balloonPara.innerHTML = "Done";
+        balloonDiv.removeEventListener('keyup', balloonChange);
+    } else if (varSize==6) {
+        balloonPara.innerHTML = "💥";
+        balloonDiv.removeEventListener('keyup', balloonChange);
+    } 
+})
 
 balloonDiv.addEventListener('click', test => {
     console.log('hi');
