d5de66d938a23fe1390476e3d5dda29ff5b34e3c
Quality.kun <jrgames@kaiohana.net>
2019-10-16 09:14:30 -0700

    i think thats the function part, just need size

--- code.js
+++ code.js
@@ -26,25 +26,26 @@ var balloonSize=ballonPara.style.fontSize
 }*/
 
 document.addEventListener('keyup', balloonChange => {
-    while (varSize>0 && varSize<6) {
-        if (event.code=='KeyD') {
+    if (event.code=='KeyD') {
+        if (varSize>0) {
             balloonSize=(balloonSize-10)+'px';
             varSize-=1;
             console.log(varSize)
-        } else if (event.code=='KeyI') {
+        } else {
+            balloonSize = "10pt";
+            balloonPara.innerHTML = "Done";
+            balloonDiv.removeEventListener('keyup', balloonChange);
+        }
+    } else if (event.code=='KeyI') {
+        if (varSize<6) {
             balloonSize=(balloonSize+10)+'px';
             varSize+=1;
             console.log(varSize)
+        } else {
+            balloonPara.innerHTML = "💥";
+            balloonDiv.removeEventListener('keyup', balloonChange);
         }
     }
-    if (varSize==0) {
-        balloonSize = "16pt";
-        balloonPara.innerHTML = "Done";
-        balloonDiv.removeEventListener('keyup', balloonChange);
-    } else if (varSize==6) {
-        balloonPara.innerHTML = "💥";
-        balloonDiv.removeEventListener('keyup', balloonChange);
-    } 
 })
 
 balloonDiv.addEventListener('click', test => {
